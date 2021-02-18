223b1e4aa1c960fc8924d134d47fa2fb6e35458b
a3at28 <a3at28@gmail.com>
2021-02-18 21:46:56 +0300

    incomeSber.java

---
+++ src/incomeSber.java
@@ -0,0 +1,7 @@
+public class incomeSber {
+    public static void main(String[] args) {
+        int percent = 3;
+        int depositAmount = 6000;
+        System.out.println(depositAmount / 100 *percent);
+    }
+}
